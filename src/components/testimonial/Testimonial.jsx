7841d167ef9a0f95f4d96c41edabbe49697514e5
assiayussiri659 <ayushdubey2053@gmail.com>
2024-07-14 14:42:17 +0530

    m

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -13,15 +13,15 @@ const Testimonial = () => {
 
                     <div className="flex flex-wrap -m-4">
                         {/* Testimonial 1 */}
-                        <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                        {/* <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://ecommerce-sk.vercel.app/img/kamal.png" />
                                 <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Kamal Nayan Upadhyay</h2>
-                                <p className="text-gray-500">Senior Product Designer</p>
+                                <p className="text-gray-500">Full stack Developer</p>
                             </div>
-                        </div>
+                        </div> */}
 
                         {/* Testimonial 2 */}
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
@@ -35,7 +35,7 @@ const Testimonial = () => {
                         </div>
 
                         {/* Testimonial 3 */}
-                        <div className="lg:w-1/3 lg:mb-0 p-4">
+                        {/* <div className="lg:w-1/3 lg:mb-0 p-4">
                             <div className="h-full text-center">
                                 <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://firebasestorage.googleapis.com/v0/b/devknus-official-database.appspot.com/o/images%2FScreenshot%202023-07-07%20at%202.20.32%20PM-modified.png?alt=media&token=324ddd80-2b40-422c-9f1c-1c1fa34943fa" />
                                 <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
@@ -43,7 +43,7 @@ const Testimonial = () => {
                                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">XYZ </h2>
                                 <p className="text-gray-500">CTO</p>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </section>
